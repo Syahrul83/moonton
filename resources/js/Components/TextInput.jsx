f3dd92c88d80d5798c9540d0c81afb57243f2a4c
syahrul anwar <syahrulanwar83@gmail.com>
2023-01-12 10:17:06 +0800

    feat: slicing 2

--- resources/js/Components/TextInput.jsx
+++ resources/js/Components/TextInput.jsx
@@ -1,33 +1,47 @@
-import { forwardRef, useEffect, useRef } from 'react';
+import { forwardRef, useEffect, useRef } from 'react'
 
 export default forwardRef(function TextInput(
-    { type = 'text', name, id, value, className, autoComplete, required, isFocused, handleChange },
-    ref
+  {
+    type = 'text',
+    id,
+    name,
+    defaultValue,
+    variant = 'primary',
+    placeholder,
+    isError,
+    value,
+    className,
+    autoComplete,
+    required,
+    isFocused,
+    handleChange,
+  },
+  ref,
 ) {
-    const input = ref ? ref : useRef();
+  const input = ref ? ref : useRef()
 
-    useEffect(() => {
-        if (isFocused) {
-            input.current.focus();
+  useEffect(() => {
+    if (isFocused) {
+      input.current.focus()
+    }
+  }, [])
+
+  return (
+    <div className="flex flex-col items-start">
+      <input
+        type={type}
+        name={name}
+        id={id}
+        value={value}
+        className={
+          'rounded-2xl bg-form-bg py-[13px] px-7 w-full focus:outline-alerange focus:outline-none input-${variant} ${className}'
         }
-    }, []);
-
-    return (
-        <div className="flex flex-col items-start">
-            <input
-                type={type}
-                name={name}
-                id={id}
-                value={value}
-                className={
-                    `border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm ` +
-                    className
-                }
-                ref={input}
-                autoComplete={autoComplete}
-                required={required}
-                onChange={(e) => handleChange(e)}
-            />
-        </div>
-    );
-});
+        ref={input}
+        autoComplete={autoComplete}
+        required={required}
+        onChange={(e) => handleChange(e)}
+        placeholder={placeholder}
+      />
+    </div>
+  )
+})
