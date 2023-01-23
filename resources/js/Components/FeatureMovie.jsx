84fd2f6f06c8b98e5ef8720b217c17fe7274053d
syahrul anwar <syahrulanwar83@gmail.com>
2023-01-23 19:34:18 +0800

    hotfix : movie card

--- resources/js/Components/FeatureMovie.jsx
+++ resources/js/Components/FeatureMovie.jsx
@@ -11,7 +11,7 @@ export default function FeatureMovie({
     <div className="absolute overflow-hidden group mr-[30px]">
       <img
         // src="/images/featured-1.png"
-        src={`/storage/${thumbnail}`}
+        src={`/storage/${thumbnail??''}`}
         className="object-cover rounded-[30px] w-[520px] h-[340px]"
         alt=""
       />
