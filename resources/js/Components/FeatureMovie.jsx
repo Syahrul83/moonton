e713cc00a646db05e17a8c22072bb8772ac4552c
syahrul anwar <syahrulanwar83@gmail.com>
2023-01-27 10:04:09 +0800

    fix rating feature movie

--- resources/js/Components/FeatureMovie.jsx
+++ resources/js/Components/FeatureMovie.jsx
@@ -16,11 +16,11 @@ export default function FeatureMovie({
         alt=""
       />
       {/* <!-- rating --> */}
-      <div className="rating absolute top-0 left-0">
+      <div className="absolute top-0 left-0 rating">
         <div className="p-[30px] flex items-center gap-1">
           <img src="/icons/ic_star.svg" alt="" />
-          <span className="text-sm font-medium text-white mt-1">
-            {rating.toFixed(1)}/5.0
+          <span className="mt-1 text-sm font-medium text-white">
+            {parseFloat(rating).toFixed(1)}/5.0
           </span>
         </div>
       </div>
@@ -31,7 +31,7 @@ export default function FeatureMovie({
       >
         <div>
           <div className="font-medium text-[22px] text-white">{name}</div>
-          <p className="mb-0 text-white text-sm font-light">{category}</p>
+          <p className="mb-0 text-sm font-light text-white">{category}</p>
         </div>
         <div className="translate-x-[100px] group-hover:translate-x-0 transition ease-in-out duration-500">
           <img src="/icons/ic_play.svg" width="50" alt="" />
@@ -40,7 +40,7 @@ export default function FeatureMovie({
       <Link
         href={route('user.dashboard.movie.show', slug)}
         method="get"
-        className="inset-0 absolute z-50"
+        className="absolute inset-0 z-50"
       ></Link>
     </div>
   )
